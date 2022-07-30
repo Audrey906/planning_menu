8cdd6231d8531c8ddbec8fa5e3afc5cb1e39e6bc
Audrey <audrey.thirard@hotmail.fr>
2022-07-30 17:22:18 +0200

    change dish

---
+++ src/Controller/PlanningDetailController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PlanningMenuDetail;
+use App\Repository\DayRepository;
+use App\Repository\DishRepository;
+use App\Repository\PlanningMenuDetailRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PlanningDetailController extends AbstractController
+{
+    /**
+     * @Route("/planning/detail/update/dish", name="planning_detail_update_dish",methods={"POST"})
+     */
+    public function updateDish(Request $request, PlanningMenuDetailRepository $planningDetailRepo, DishRepository $dishRepo, DayRepository $dayRepo): Response
+    {
+        $plannindDetailId = $request->request->get('idPlanning');
+        $planningDetail = $planningDetailRepo->find($plannindDetailId);
+
+        $newDishId = $request->request->get('newDish');
+        $dish = $dishRepo->find($newDishId);
+
+        // update planningDetail
+        $planningDetail->setDish($dish);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($planningDetail);
+        $entityManager->flush();
+
+        // info for render
+        $planning = $planningDetail->getPlanningMenu();
+        $detail = $planning->getPlanningMenuDetails()->getValues();
+        $dishes = $dishRepo->findBy(['user' => $this->getUser()]);
+        $days = $dayRepo->findAll();
+
+        return $this->render('planning/show.html.twig', [
+            'planning' => $planning,
+            'detail' => $detail,
+            'dishes' => $dishes,
+            'days' => $days,
+        ]);
+    }
+}
