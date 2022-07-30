44aa6d79f66c80601e93890f3d741f82e41f1297
Audrey <audrey.thirard@hotmail.fr>
2022-07-30 19:45:29 +0200

    exception namespace

--- src/Exception/NotAuthaurized.php
+++ src/Exception/NotAuthaurized.php
@@ -1,7 +1,11 @@
 <?php
 
-namespace src\Exception;
+namespace App\Exception;
 
 class NotAuthaurized extends \Exception
 {
+    public function __construct($message, $code = 401, \Throwable $previous = null) {
+
+        parent::__construct($message, $code, $previous);
+      }
 }
