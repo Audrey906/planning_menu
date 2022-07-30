7c2fd250c735813f1ad21a6dd81de459a387df99
Audrey <audrey.thirard@hotmail.fr>
2022-07-30 17:27:11 +0200

    change dish

--- public/js/printer.js
+++ public/js/printer.js
@@ -5,7 +5,7 @@ function print() {
 
     $(".col-md-1").css("min-width", "130px");
 	content.document.open();
-    header = '<head><script src="https://kit.fontawesome.com/8a9e68092d.js" crossorigin="anonymous"></script> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> <link rel="stylesheet" href="\'../../css/style.css\'"><\head>'
+    header = '<head><script src="https://kit.fontawesome.com/8a9e68092d.js" crossorigin="anonymous"></script> <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"> <link rel="stylesheet" href="\'../../css/style.css\'"></head>'
 	content.document.write('<html>' + header + '<body onload="window.print()">' + infoToPrint.innerHTML + '</html>');
 	content.document.close();
 
